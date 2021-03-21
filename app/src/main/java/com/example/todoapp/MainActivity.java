fa5fa28b86018d05c5b9749f6e6be2392b04fe22
Sakriya Bajracharya <sakriya33@gmail.com>
2021-03-21 08:42:13 +0545

    todo : add insert query and form

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.todoapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.todoapp.data.Repository;
 import com.example.todoapp.data.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
@@ -16,16 +19,25 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerview;
     private TaskAdapter adapter;
     Repository repository;
-
+    private FloatingActionButton addButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    repository = Repository.getRepository(this.getApplication());
+        repository = Repository.getRepository(this.getApplication());
         recyclerview = findViewById(R.id.tasklist);
         List<Task> tasks = repository.getAllTasks();
         adapter = new TaskAdapter(tasks);
         recyclerview.setAdapter(adapter);
 
+
+        addButton = findViewById(R.id.add_btn);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
